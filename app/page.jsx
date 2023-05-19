65f623f3db8298b3d7c0c4c50d8e7a07009ffd59
HJ-David <david.hoonjin.jang@gmail.com>
2023-05-19 09:57:04 -0700

    minor updates

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,14 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> One-liner that will make smile</span>
+      <span className='orange_gradient text-center'>
+        {" "}
+        One-liner that will make you smile
+      </span>
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      One-liner is all about witty and humorous words, encouraging thoughts,
+      insightful perspectives that can give you positive energy.
     </p>
 
     <Feed />
